6908864cd544fe95fc0f4f110f9bfc19d7d321da
Luke.Humphreys <lhumphreys67@gmail.com>
2019-10-25 08:39:31 +0100

    Additional test cases:
       - Simple static library, which requires to .gcov files to be merged
         in the GCOV workflow, and for multiple records to be merged in the
         LCOV workflow
       - Simple template library, which requires to .gcov files to be merged
         in the GCOV workflow, and for multiple records to be merged in the
         LCOV workflow
       - Out of tree test, where --root is used to specify a separate source
         directory as the base file path
       - Missing files test - which ensures that files are reported as
         missing, even if it hasn't been linked into any test
       - File / directory exlcusion patterns (gcov only)
       - File / directory explicit inclusions (gcov only)

--- test.bash
+++ test.bash
@@ -19,6 +19,12 @@ function TestFailed {
 }
 
 testDir.sh $@ test-src/simple || TestFailed
+testDir.sh $@ test-src/included_files || TestFailed
+testDir.sh $@ test-src/excluded_files || TestFailed
+testDir.sh $@ test-src/static_lib || TestFailed
+testDir.sh $@ test-src/out_of_tree  || TestFailed
+testDir.sh $@ test-src/missing_files  || TestFailed
+testDir.sh $@ test-src/template_includes/ || TestFailed
 
 if [[ $failed_tests -gt 0 ]]; then
     echo ""
